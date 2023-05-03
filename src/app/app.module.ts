7b753921e595c95bbd354be09b998c85b1108290
marufa <marufa.fm@gmail.com>
2023-05-03 18:21:43 +0600

    card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
+import { PropertyListComponent } from './property-list/property-list.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/property-list/property-list.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-list',
+  templateUrl: './property-list.component.html',
+  styleUrls: ['./property-list.component.css']
+})
+export class PropertyListComponent implements OnInit {
+  constructor (private http:HttpClient){}
+  ngOnInit(): void {
+    this.http.get('data/properties.json').subscribe(
+      data=>{
+        console.log(data);
+      }
+    )
+  }
+}
